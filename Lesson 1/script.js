abd482b7618859e814b603bef3a9df3115c8e5df
Dmitry Tsyrulik <dima.tsyrulik@gmail.com>
2019-05-01 21:47:00 +0300

    first homeTask

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,54 +1,26 @@
 'use strict';
+//2
+let money = prompt("Ваш бюджет на месяц?",  "");
+let time = prompt("Введите дату в формате YYYY-MM-DD","");
 
-a = 5;
-console.log(a);
+//4
+let mandatoryArticly = prompt("Введите обязательную статью расходов в этом месяце", "");
+let howMuch = prompt("Во сколько обойдется?");
+//3
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        mandatory: mandatoryArticly,
+        how: howMuch
+    },
+    optionalExpenses: {
 
-var number = 5;
-var string = "Hello";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+    },
+    income: {
 
-
-console.log(4 / 0);
-console.log("string" * 9);
-
-let something;
-console.log(something);
-
-let person = {
-    name: "Jhon",
-    age: 25,
-    isMaried: false
+    },
+    savings: false
 }
-console.log(person["name"]);
 
-let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
-console.log(arr[0])
-
-alert("Hello world!");
-let answer = confirm("Are you here?");
-console.log(answer);
-
-// let answer = +prompt("Есть ли вам 18?", "Да");
-// console.log(typeof(answer));
-
-console.log("arr" + " - object");
-console.log(4 + +' - object');
-
-let incr = 10,
-    decr = 10;
-
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5 % 2);
-console.log("2" == 2);
-
-let isChecked = false,
-    isClose = false;
-
-console.log(isChecked || isClose);
+alert(appData.budget / 30);
