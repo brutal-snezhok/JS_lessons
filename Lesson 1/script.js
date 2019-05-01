ab64005496b557907fb9300bf64cae95bdead2ae
Dmitry Tsyrulik <dima.tsyrulik@gmail.com>
2019-05-01 21:30:28 +0300

    Uncommented code

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -3,40 +3,40 @@
 a = 5;
 console.log(a);
 
-// var number = 5;
-// var string = "Hello";
-// var sym = Symbol();
-// var boolean = true;
-// null;
-// undefined;
-// var obj = {};
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
 
 
-// console.log(4 / 0);
-// console.log("string" * 9);
+console.log(4 / 0);
+console.log("string" * 9);
 
-// let something;
-// console.log(something);
+let something;
+console.log(something);
 
-// let person = {
-//     name: "Jhon",
-//     age: 25,
-//     isMaried: false
-// }
-// console.log(person["name"]);
+let person = {
+    name: "Jhon",
+    age: 25,
+    isMaried: false
+}
+console.log(person["name"]);
 
-// let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
-// console.log(arr[0])
+let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
+console.log(arr[0])
 
-//alert("Hello world!");
-// let answer = confirm("Are you here?");
-// console.log(answer);
+alert("Hello world!");
+let answer = confirm("Are you here?");
+console.log(answer);
 
 // let answer = +prompt("Есть ли вам 18?", "Да");
 // console.log(typeof(answer));
 
-// console.log("arr" + " - object");
-// console.log(4 + +' - object');
+console.log("arr" + " - object");
+console.log(4 + +' - object');
 
 let incr = 10,
     decr = 10;
