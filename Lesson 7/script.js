e03c0bac309090e2f0200c6a7fa413b5f561c426
Dmitry Tsyrulik <dima.tsyrulik@gmail.com>
2019-06-22 16:36:35 +0300

    Promises

---
+++ Lesson 7/script.js
@@ -0,0 +1,34 @@
+let drink = 1;
+
+function shoot(arrow){
+    console.log("Вы сделали выстрел...");
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            Math.random() > .5 ? resolve() : reject("Промах(");
+        }, 3000);
+    });  
+
+    return promise;
+};
+
+function win(){
+    console.log("Вы попедили");
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer(){
+    console.log("Вам купили колу");
+}
+
+function giveMoney(){
+    console.log("Вам заплатили");
+}
+
+function loose(){
+    console.log("Вы проиграли");
+}
+
+shoot({})
+        .then(mark => console.log("Вы попали в цель!"))
+        .then(win)
+        .catch(loose)
